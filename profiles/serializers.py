d03f73bb90aebf062b426eebc11160da6fe66cdf
johnston9 <davejohnston9@gmail.com>
2021-11-15 15:00:10 +0000

    following id

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,21 +3,33 @@ Serializer for Profiles App
 """
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     """ Serializer class for Profiles App """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticate:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            print(following)
+            return following.id if following else None
+        return None
 
     class Meta:
         """ Meta for Profiles Serializer """
         model = Profile
         fields = [
             'id', 'owner', 'position', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner'
+            'content', 'image', 'is_owner', 'following_id',
         ]
