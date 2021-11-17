de3a7b303a69eb132c46e6f4915f801f4293da43
johnston9 <davejohnston9@gmail.com>
2021-11-17 20:19:46 +0000

    logout

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,6 +1,7 @@
 """
 Serializer for Profiles App
 """
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 
@@ -13,10 +14,18 @@ class CommentSerializer(serializers.ModelSerializer):
     name = serializers.ReadOnlyField(source='owner.profile.name')
     position = serializers.ReadOnlyField(source='owner.profile.position')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     class Meta:
         """ Meta for Comments Serializer """
