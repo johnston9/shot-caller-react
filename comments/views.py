7cf3d0660181c7bc37d2adb66d1b6ade02011c2d
johnston9 <davejohnston9@gmail.com>
2021-11-17 19:18:21 +0000

    JWT

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,39 @@
+""" Generic comment views """
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions
+from shot_caller_react.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
+
+
+class CommentList(generics.ListCreateAPIView):
+    """
+    List comments or create a comment if logged in.
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+    filter_backends = [
+        DjangoFilterBackend
+    ]
+    filterset_fields = [
+        'post',
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a comment, or update or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
+
+
+
 # """ Comments views """
 # from django.http import Http404
 # from rest_framework import status, permissions
@@ -78,30 +114,3 @@
 #         return Response(
 #             status=status.HTTP_204_NO_CONTENT
 #         )
-
-
-from rest_framework import generics, permissions
-from shot_caller_react.permissions import IsOwnerOrReadOnly
-from .models import Comment
-from .serializers import CommentSerializer, CommentDetailSerializer
-
-
-class CommentList(generics.ListCreateAPIView):
-    """
-    List comments or create a comment if logged in.
-    """
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Comment.objects.all()
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-
-class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Retrieve a comment, or update or delete it by id if you own it.
-    """
-    permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = CommentDetailSerializer
-    queryset = Comment.objects.all()
