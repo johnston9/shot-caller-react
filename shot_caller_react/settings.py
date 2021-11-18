daa32ef309545e3034ba40b904347bf29d2d73a8
johnston9 <davejohnston9@gmail.com>
2021-11-18 19:51:36 +0000

    regex

--- shot_caller_react/settings.py
+++ shot_caller_react/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
+import re
 import dj_database_url
 if os.path.exists('env.py'):
     import env
@@ -64,15 +65,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    'shot-caller-api.herokuapp.com',
+    os.environ.get('ALLOWED_HOST'),
     'localhost',
 ]
-
-# if 'CLIENT_ORIGIN' in os.environ:
-#     CORS_ALLOWED_ORIGINS = [
-#         os.environ.get('CLIENT_ORIGIN'),
-#         os.environ.get('CLIENT_ORIGIN_DEV'),
-#     ]
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
@@ -81,6 +76,12 @@ if 'CLIENT_ORIGIN' in os.environ:
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
         r"^https://.*\.gitpod\.io$",
+    ]
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
