4e1db76532379ab5fb643baa20bd137224727f6e
johnston9 <davejohnston9@gmail.com>
2021-12-17 15:39:24 +0000

    search filters

--- scenes/views.py
+++ scenes/views.py
@@ -1,8 +1,10 @@
 """ Views for Scenes App """
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from shot_caller_react.permissions import IsOwnerOrReadOnly
 from .models import Scene
 from .serializers import SceneSerializer
+
 
 class ScenesList(generics.ListCreateAPIView):
     """ List all Scenes """
