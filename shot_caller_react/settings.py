1c1791dae3ca70c660a53a74ecf88404d7559493
johnston9 <davejohnston9@gmail.com>
2022-07-08 18:04:09 +0000

    Changed cookies names

--- shot_caller_react/settings.py
+++ shot_caller_react/settings.py
@@ -46,8 +46,8 @@ if 'DEV' not in os.environ:
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
-JWT_AUTH_COOKIE = 'my-app-auth'
-JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_COOKIE = 'my-shot-caller-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-shot-caller-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 
