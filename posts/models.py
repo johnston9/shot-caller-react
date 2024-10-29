67c9c4643ab8d236f365ab5ae1e60f0b3901297e
johnston9 <davejohnston9@gmail.com>
2024-10-29 20:10:27 +0000

    Department view alter search

--- posts/models.py
+++ posts/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
         ('camera', 'Camera'), ('script', 'Script'), ('art', 'Art'),
         ('make-up', 'Hair/Makeup'), ('wardrobe', 'Wardrobe'),
         ('location', 'Location'), ('sound', 'Sound'),
-        ('casting', 'Casting'), ('post', 'Post/FXS'),
+        ('casting', 'Casting'), ('post', 'Post/VFX'),
         ('production', 'Production'), ('stunts', 'Stunts'),
         ('electric', 'Electric/Grip'), ('universal', 'Universal'),
      ]
